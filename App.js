95c3d98a100904ec7f658eb2bb1e9a419d667c4e
Nikolas Wijaya <ayannahnwijaya@ayannah.com>
2020-07-29 13:58:18 +0700

    init first

---
+++ App.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import {
+  View,
+  Text, StyleSheet
+} from 'react-native';
+
+
+const App= () =>  {
+  return (
+    <View>
+      <Text>My Doctor App</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
